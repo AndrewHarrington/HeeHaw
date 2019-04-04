ad63e3b2124f00c3733256aa866791f258dab344
Andrew Harrington <aharrington4@mail.greenriver.edu>
2019-04-04 13:54:19 -0400

    Moved all JavaScript code into a seperate file

---
+++ HeeHaw.js
@@ -0,0 +1,19 @@
+//from 1 through 1000
+for (var i = 1; i <= 1000; i++) {
+    //check for Hee
+    if(i%3 == 0 && i%5 != 0){
+        console.log("Hee");
+    }
+    //check for Haw
+    if(i%5 == 0 && i%3 != 0){
+        console.log("Haw");
+    }
+    //check for both
+    if(i%3 == 0 && i%5 == 0){
+        console.log("Hee Haw")
+    }
+    //other
+    if(i%3 != 0 && i%5 != 0){
+        console.log(i);
+    }
+}
